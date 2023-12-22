7a8ec6cfed3984521329b410654a6a707c0f8261
macaroni-waffles <115866814+Macaroni-Waffles@users.noreply.github.com>
2023-12-22 18:04:57 +0000

    practice js functions

--- javascript.js
+++ javascript.js
@@ -11,8 +11,6 @@
 //alert(color)
 
 
-
-
 //Declare two variables: admin and name.
 //Assign the value "John" to name.
 //Copy the value from name to admin.
@@ -25,9 +23,71 @@
 //alert(admin);
 
 
-
 //Create a variable with the name of our planet. How would you name such a variable?
 //Create a variable to store the name of a current visitor to a website. How would you name that variable?
 
-let ourPlanet = "Earth";
-let currentVisitorName;
+//let ourPlanet = "Earth";
+//let currentVisitorName;
+
+
+//console.log(23 + 97);
+//console.log(1 + 2 + 3 + 4 + 5 + 6);
+//console.log((4 + 6 + 9) / 77);
+
+//let a = 10;
+//console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Write a function called add7 that takes one number and returns that number + 7.
+
+function add7 (number) {
+    return result = number + 7
+    console.log(add7)
+}
+
+//Write a function called multiply that takes 2 numbers and returns their product.
+
+function multiply (number1, number2) {
+    return result = number1 * number2
+    console.log(multiply)
+}
+
+//Write a function called capitalize that takes a string and returns that string with only the first letter capitalized. Make sure that it can take strings that are lowercase, UPPERCASE or BoTh.
+
+
+function capitalize (string) {
+    let lowercase = string.toLowerCase();
+    let firstLetter = lowercase[0].toUpperCase();
+    return firstLetter + lowercase.substring(1);
+    console.log(capitalize)
+}
+
+
+//function capitalize () {
+ //   let stringLower = HOWDY.lowercase()
+ //   return stringLower
+  //  let stringFirstUpper = stringLower.toUpperCase()
+   //  console.log(capitalize)}
